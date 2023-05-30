accc8a9e5057ab52d8082ddc9b492163fff6b6b4
Eric Bridgeford <ericbridgeford@erics-mbp.mynetworksettings.com>
2023-05-30 18:01:48 -0400

    Update config stuff

--- service/dunnit-graphql/src/index.ts
+++ service/dunnit-graphql/src/index.ts
@@ -1,12 +1,12 @@
-import { MikroORM } from '@mikro-orm/core';
-import { __prod__ } from './constants';
-import { Post } from './entities/Post';
-import microConfig from "./mikro-orm.config"
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import config from "./mikro-orm.config";
 
 const main = async () => {
-  const orm = await MikroORM.init(microConfig);
+  const orm = await MikroORM.init(config);
 
-  const post = orm.em.create(Post, {title: 'ibibibqeuibevb'});
-  orm.em.persistAndFlush(post)
+  const post = orm.em.create(Post, { title: "ibibibqeuibevb" });
+  orm.em.persistAndFlush(post);
 };
 main();
--- service/dunnit-graphql/src/mikro-orm.config.ts
+++ service/dunnit-graphql/src/mikro-orm.config.ts
@@ -1,10 +1,14 @@
-import { __prod__ } from './constants';
-import { Post } from './entities/Post';
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import { Options } from "@mikro-orm/core";
 
-export default {
+const config: Options = {
   entities: [Post],
-  dbName: 'dunnit',
-  user: 'postgres',
-  password: 'postgres',
+  dbName: "dunnit",
+  type: "postgresql",
+  user: "user",
+  password: "password",
   debug: !__prod__,
 };
+
+export default config;
